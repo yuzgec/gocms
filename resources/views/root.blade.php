25e588f0dc5ec7d09455fbaf2b2af07a863193e5
yuzgec <olcayy@gmail.com>
2022-10-27 22:33:40 +0300

    Fix

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -6,7 +6,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-        @vite(['resources/js/app.js'])
         @spladeHead
     </head>
     <body class="font-sans antialiased">
